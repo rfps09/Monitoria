26c5d7a9d7d5cfd2a7d2389e8db3b0217ecd0229
rfps09 <rafael09felipe@gmail.com>
2022-08-10 09:39:44 -0300

    Perceptron AND

---
+++ PerceptronSimples/AND.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+numEpocas = 6
+q = 4
+
+X1 = np.array([0,0,1,1])
+X2 = np.array([0,1,0,1])
+X = np.vstack((X1,X2))
+Y = np.array([0,0,0,1])
+W = np.zeros(3)
+bias = 1
+eta = 0.1
+e = np.zeros(q)
+
+for j in range(numEpocas):
+    for i in range(q):
+        Xb = np.hstack((bias, X[:,i]))
+        V = np.dot(W, Xb)
+        Yr = np.heaviside(V,0)
+        e[i] = Y[i]-Yr
+        W = W + eta*e[i]*Xb
+        
+print("O vetor de erros (e) = " + str(e))
