06aff418f0f4e9855d42319f4000f0233e32470d
rfps09 <rafael09felipe@gmail.com>
2022-10-03 13:25:02 -0300

    att

--- TensorFlow/testes/grafico.py
+++ TensorFlow/testes/grafico.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-numEpocas = 4
+numEpocas = 80
 q = 4
 
 X1 = np.array([0,0,1,1])
 X2 = np.array([0,1,0,1])
 X = np.vstack((X1,X2))
 Y = np.array([0,1,1,1])
+Yd = np.zeros(4)
 W = np.array([0,1,-1])
 W = np.zeros(3)
 e = np.zeros(q)
 bias = 1
-eta = 0.1
+eta = 0.3
 
 def draw_decision_boundary(a,b,c):
     space = np.linspace(-10,10,100)
@@ -33,18 +34,18 @@ def draw_decision_boundary(a,b,c):
     plt.show()
 
 def funcaoDeAtivicao(valor):
-    if valor < 0.0:
-        return 0
-    return 1
+    return 1/(1+np.exp(-valor))
 
 for j in range(numEpocas):
     for i in range(q):
         Xb = np.hstack((bias, X[:,i]))
         V = np.dot(W,Xb)
-        print(V)
         Yr = funcaoDeAtivicao(V)
-        e[i] = Y[i] - Yr
-        W = W + eta*e[i]*Xb
-        draw_decision_boundary(W[1],W[2],W[0])
+        Yd[i] = Yr
+        er = 2*(Y[i]-Yr)*(Yr*(1-Yr))*Xb
+        W = W + eta*er
+        e[i] = np.power(Y[i] - Yr,2)/2
 
+print(Yd)
+draw_decision_boundary(W[1],W[2],W[0])
 print("O vetor de erros (e) = " + str(e))
